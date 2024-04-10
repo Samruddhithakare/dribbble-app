31119b17d73215ca9eb572c832aef00d86344dcd
Samruddhithakare <samruddhipthakare@gmail.com>
2024-04-10 20:57:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignupPage from './components/SignupPage';
+import CreateProfilePage from './components/CreateProfilePage';
+import QuestionPage from './components/QuestionPage';
+import VerifyEmailPage from './components/VerifyEmailPage';
+import { useState } from 'react';
 
 function App() {
+  
+  const [profilePic,setProfilePic]=useState(null); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignupPage />} />
+        <Route path="/create-profile"  element={<CreateProfilePage/>} />
+        <Route path="/questions" element={<QuestionPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
